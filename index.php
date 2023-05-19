078fa8e34440054d270f228c6bfe0ba277b71ad2
Ahmed-Hemida <Ahmed-Hemida>
2023-05-19 22:19:13 +0300

    add dockeFile

--- index.php
+++ index.php
@@ -3,45 +3,48 @@
 include('Controllers/controller.php');
 $filename =  $_SERVER['REQUEST_URI'];
  $auth= (isset($_SESSION['Auth'])&&$_SESSION['Auth'])?true:false;
-
-switch($filename){
-    case "/check-twitter-user/registration": 
+ $project_name = basename(dirname(_FILE_));
+ $auth= (isset($_SESSION['Auth'])&&$_SESSION['Auth'])?true:false;
+ $url=preg_replace('/^\/'.$project_name.'/','',$filename);
+ 
+switch($url){
+    case "/registration": 
         require __DIR__ . '/view/registration.html';
     break;
-    case "/check-twitter-user/": 
+    case "/": 
         require __DIR__ . '/view/login.html';
     break;
-    case "/check-twitter-user/profile": 
-        if(!$auth)header("Location: /check-twitter-user/");
+    case "/profile": 
+        if(!$auth)header("Location: /");
     require __DIR__ . '/view/profile.php';
     break;
-    case "/check-twitter-user/logOut": 
-        if(!$auth)header("Location: /check-twitter-user/");
+    case "/logOut": 
+        if(!$auth)header("Location: /");
         controller::logOut();
     break;
-    case "/check-twitter-user/login": 
+    case "/login": 
         controller::login();
-        if(!$auth)header("Location: /check-twitter-user/");
+        if(!$auth)header("Location: /");
     break;
-    case "/check-twitter-user/feedback": 
-        if(!$auth)header("Location: /check-twitter-user/");
+    case "/feedback": 
+        if(!$auth)header("Location: /");
         controller::createFeedback();
     break;
-    case "/check-twitter-user/get/user/feedback": 
-        if(!$auth)header("Location: /check-twitter-user/");
+    case "/get/user/feedback": 
+        if(!$auth)header("Location: /");
         controller::getUserFeedback();
     break;
-    case "/check-twitter-user/create/user": 
+    case "/create/user": 
         controller::createUser();
     break;
-    case "/check-twitter-user/migration": 
+    case "/migration": 
         controller::migration();
     break;
-    case "/check-twitter-user/Drop/DataBase/": 
+    case "/Drop/DataBase/": 
         controller::DrobDB();
     break;
     default:
-    if(!$auth)header("Location: /check-twitter-user/");
+    if(!$auth)header("Location: /");
     break;
 
 }
